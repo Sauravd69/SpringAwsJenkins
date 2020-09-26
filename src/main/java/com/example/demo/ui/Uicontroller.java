22e3637630fd9e689da68ef40ca02a026e50be03
saurav <sdas31@uic.edu>
2020-09-25 22:56:09 -0500

    initial commit

---
+++ src/main/java/com/example/demo/ui/Uicontroller.java
@@ -0,0 +1,12 @@
+package com.example.demo.ui;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Uicontroller {
+	
+	public String index()
+	{
+		return "index";
+	}
+}
