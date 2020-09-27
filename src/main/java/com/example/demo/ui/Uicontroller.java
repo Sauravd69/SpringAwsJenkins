3942a6173c9852646ce3d2874d940ee7c4d1f14b
saurav <sdas31@uic.edu>
2020-09-26 20:49:34 -0500

    2nd commit

--- src/main/java/com/example/demo/ui/Uicontroller.java
+++ src/main/java/com/example/demo/ui/Uicontroller.java
@@ -1,12 +1,14 @@
 package com.example.demo.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Uicontroller {
 	
+	@RequestMapping("/")
 	public String index()
 	{
-		return "index";
+		return "index.html";
 	}
 }
